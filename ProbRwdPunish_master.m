8a8c5314eab16517c7898e6c77746806490112aa
Steven Errington <steven.errington@outlook.com>
2023-07-21 11:38:32 -0500

    Tidied main script

--- ProbRwdPunish_master.m
+++ ProbRwdPunish_master.m
@@ -1,61 +1,32 @@
-% Setup workspace
-clear all; close all; clc
-dirs = get_dirs_probrwdpunish('home');
+%% ProbRwdPunish main sheet
+
+%% Setup workspace
+clear all; close all; clc; warning off
+dirs = get_dirs_probrwdpunish('mac');
 
 % Define and load example datafile
-datafile = 'ProbRwdPunish_18_07_2023_10_58';
+datafile = 'ProbRwdPunish_21_07_2023_10_28';
 data = load(fullfile(dirs.data,datafile));
 
-<<<<<<< Updated upstream
-% In development: merge multiple files from one day - dev_mergeFiles
-
-% Extract key session information for processing
-=======
+%% Processing
 % Get trial event times and trial information
->>>>>>> Stashed changes
 [trialEventTimes, trialInfo] = get_trial_timeinfo(data.PDS);
+
+% Get choice specific information
 [choice_info] = get_choice_info(data.PDS, trialInfo);
-<<<<<<< Updated upstream
+% Clean/remove no-option trials
 choice_info = clean_choice_info(choice_info);
-=======
-rwd_punish_values = get_unique_rwdpunish(choice_info);
 
-
-
-
-
-[p_choice_feature] = get_pchoice_feature(choice_info);
->>>>>>> Stashed changes
-
+%% Exports
 % Plot daily session behavior
 plot_session_beh(choice_info,data,datafile);
 
 % Plot daily session glm
 glm_out = plot_session_glm(choice_info);
 
-<<<<<<< Updated upstream
 %% Legacy
 %  probrwdpunish_workspace
-=======
 
+% In development: merge multiple files from one day - dev_mergeFiles
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-%% Legacy
-% [option_info] = get_option_info_glm(data.PDS, trialInfo);
-% glmModel = get_task_glm(option_info);
->>>>>>> Stashed changes
 
--- get_dirs_probrwdpunish.m
+++ get_dirs_probrwdpunish.m
@@ -4,18 +4,12 @@ switch user
     case 'wustl'
         dirs.root = 'C:\Users\Steven\Documents\GitHub\2023-probrwdpunish';
         dirs.toolbox = 'C:\Users\Steven\Documents\GitHub\toolbox\';
-<<<<<<< Updated upstream
         dirs.data = 'Y:\MONKEYDATA\Slayer2\ProbRwdPunish';
 
-=======
-    case 'home'
-        dirs.root = 'D:\projects\2023-probrwdpunish';
-        dirs.toolbox = 'D:\projects\toolbox\';
->>>>>>> Stashed changes
     case 'mac'
         dirs.root = '/Users/stevenerrington/Desktop/Projects/2023-probrwdpunish';
         dirs.toolbox = '/Users/stevenerrington/Desktop/Projects/toolbox';        
-        dirs.toolbox = '/Users/stevenerrington/Box Sync/Research/2023-probrwdpunish/data';    
+        dirs.data = '/Volumes/Share2/MONKEYDATA/Slayer2/ProbRwdPunish';    
         
     case 'home'
         dirs.root = 'D:\projects\2023-probrwdpunish';

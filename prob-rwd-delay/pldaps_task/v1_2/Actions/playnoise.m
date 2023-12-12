049ba5a9ef59a0b4761af233c31ce180729a7b72
Steven Errington <steven.errington@outlook.com>
2023-12-12 14:33:02 -0600

    Remove pldaps tasks (analysis only)

--- prob-rwd-delay/pldaps_task/v1_2/Actions/playnoise.m
+++
@@ -1,5 +0,0 @@
-function [PDS, c, s] = playnoise(PDS, c, s)
-Datapixx('SetAudioSchedule', 0, c.freq, c.nTF, c.lrMode, c.noisebuffadd, c.nTF);
-Datapixx('StartAudioSchedule');
-Datapixx('RegWrRd');
-end
